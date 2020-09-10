36b04bc4bf0b9fd7042e75fc9c0c921403468d1c
aryavorskiy <iavorskii.ar@phystech.edu>
2020-09-10 20:21:43 +0300

    Improve main.py

--- LinearAlgebraApproach/main.py
+++ LinearAlgebraApproach/main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import os
 from LinearAlgebraApproach.SingleDimPhysicalSystem import SingleDimPhysicalSystem
 from Physics.Models import Coulomb
 from LinearAlgebraApproach.Mesh import plain_mesh
@@ -11,7 +12,17 @@ energy = -3000
 r = 5
 
 bounds = (0.01, 5)
-step_values = [0.0005]
+step_values = plain_mesh((0.0005, 0.005), count=500)
+
+free_dir = 0
+
+compose_pathname = lambda index: f'img_n{len(step_values)}_{index}'
+
+while os.path.exists(compose_pathname(free_dir)):
+    free_dir += 1
+
+dirname = compose_pathname(free_dir)
+os.mkdir(dirname)
 
 nrgl_x = []
 nrgl_y = []
@@ -41,8 +52,10 @@ for step in step_values:
     for i in range(idx - r, idx + r + 1):
         print('>' if i == idx else ' ', numerically_obtained[i])
 
-    ground_state.plot(filename=f'step_{step}.png', title=f'Mesh step: {step}\nState energy: {ground_energy}')
+    ground_state.plot(filename=os.path.join(dirname, f'step_{step}.png'),
+                      title=f'Mesh step: {step}\nState energy: {ground_energy}')
     print()
 
 ax = pl.axes(xlabel='d', ylabel='E')
 ax.plot(nrgl_x, nrgl_y, 'bo', ms=2)
+pl.savefig(os.path.join(dirname, f'spectre.png'))
