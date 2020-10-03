Fix torque operator formula
! Fix torque operator
+ Solution alias is now stored in the SchrodingerSolution instance ans is accessible via str()
* Typo corrections
* Add docstrings and small improvements to plot_any method

LinearAlgebraModel/Operators.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from LinearAlgebraModel.Model.BaseOperators import LinearOperator, get_first_dif_operator_mat
+from LinearAlgebraModel.Model.BaseOperators import LinearOperator, get_first_dif_operator_mat, H
 from LinearAlgebraModel.Model.Grid import Grid
 
 
@@ -10,9 +10,9 @@ class TorqueOperator(LinearOperator):
             raise ValueError('Grid must be three-dimensional')
         dx_mat = get_first_dif_operator_mat(grid, 0)
         dy_mat = get_first_dif_operator_mat(grid, 1)
-        x_mat = np.diagflat(list(pt[0] for pt in grid))
-        y_mat = np.diagflat(list(pt[1] for pt in grid))
-        super(TorqueOperator, self).__init__(grid, -1j * (np.dot(x_mat, dy_mat) - np.dot(y_mat, dx_mat)))
+        x_mat = np.diagflat(list(grid.point_to_absolute(pt)[0] for pt in grid))
+        y_mat = np.diagflat(list(grid.point_to_absolute(pt)[1] for pt in grid))
+        super(TorqueOperator, self).__init__(grid, -H * 1j * (np.dot(x_mat, dy_mat) - np.dot(y_mat, dx_mat)))
 
     def get_matrix(self) -> np.array:
         return self.mat

LinearAlgebraModel/Visual.py
@@ -11,15 +11,31 @@ def color_by_hue(hue):
 
 
 def plot_any(data, title=''):
+    """
+    Presents any array of data on a scatter plot
+
+    :param data: List with values to plot
+    :param title: Plot title
+    """
     pl.cla()
     pl.grid()
+    pl.xticks([])
     pl.title(title)
     ax = pl.axes()
-    ax.plot(list(range(len(data))), data)
+    ax.scatter(list(range(len(data))), data)
 
 
 class WaveFunctionVisualizer:
+    """
+    A structure that contains methods to process and visualize wave function data
+    """
+
     def __init__(self, wf: WaveFunction):
+        """
+        Creates a new WaveFunctionVisualizer instance.
+
+        :param wf: Wave function to plot
+        """
         self.wf = wf
         self.val_data = None
         self.col_data = None
@@ -29,29 +45,29 @@ class WaveFunctionVisualizer:
             for j in range(coord_mat.size):
                 coord_mat[self.wf.grid[j]] = self.wf.grid.point_to_absolute(self.wf.grid[j])[i]
 
-    def __extract_data(self, format):
+    def __extract_data(self, data_type):
         """
-        Calculate wave function data to plotable format
+        Calculate wave function data to plot data format.
 
         :return: multi-dimensional array
         """
         data = np.array(self.wf.grid.mesh())
-        if format == 'value' or format == 'val':
+        if data_type == 'value' or data_type == 'val':
             for pt in self.wf.grid:
                 data[pt] = self.wf.values[self.wf.grid.index(pt)]
-        elif format == 'phase' or format == 'phs':
+        elif data_type == 'phase' or data_type == 'phs':
             for pt in self.wf.grid:
                 data[pt] = np.angle(self.wf.values[self.wf.grid.index(pt)]) / (2 * np.pi)
-        elif format == 'prob' or format == 'pr':
+        elif data_type == 'prob' or data_type == 'pr':
             for pt in self.wf.grid:
                 data[pt] = self.wf.values[self.wf.grid.index(pt)] * np.conj(self.wf.values[self.wf.grid.index(pt)])
         else:
-            raise ValueError('Unknown data extraction argument {}'.format(format))
+            raise ValueError('Unknown data extraction argument {}'.format(data_type))
         return data
 
     def set_value_data(self, data_type: str):
         """
-        Select data to be plotted
+        Select data to be plotted.
 
         :param data_type: Data format
         :return:
@@ -60,7 +76,7 @@ class WaveFunctionVisualizer:
 
     def set_color_data(self, data_type: str):
         """
-        Select data to be shown as color
+        Select data to be shown as color.
 
         :param data_type: Data format
         :return:
@@ -70,11 +86,13 @@ class WaveFunctionVisualizer:
         for pt in self.wf.grid:
             self.col_data[pt] = color_by_hue(data[pt])
 
-    def plot(self, title='', xlabel='', ylabel=''):
+    def plot(self, title='', x_label='', y_label=''):
         """
         Plot selected data
 
         :param title: Plot title
+        :param x_label: Text label on the X axis
+        :param y_label: Text label on the Y axis
         :return:
         """
         if self.val_data is None:
@@ -85,15 +103,15 @@ class WaveFunctionVisualizer:
         args = *self.coord_mats, self.val_data
         if self.wf.grid.dimensions() == 1:
             ax = pl.axes()
-            ax.set_xlabel(xlabel)
+            ax.set_xlabel(x_label)
             if self.col_data is not None:
                 ax.scatter(*args, c=self.col_data)
             else:
                 ax.plot(*args)
         elif self.wf.grid.dimensions() == 2:
             ax = pl.axes(projection='3d')
-            ax.set_xlabel(xlabel)
-            ax.set_ylabel(ylabel)
+            ax.set_xlabel(x_label)
+            ax.set_ylabel(y_label)
             if self.col_data is not None:
                 ax.plot_surface(*args, facecolors=self.col_data)
             else: